ef6666cdb4ee0cf0e2c77c2d7883a0c643cdfa6a
Alexander Liao <alex.yj.liao@gmail.com>
2022-03-27 11:36:26 -0400

    load_all now flattens

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 import { recursive, require } from "file-ez";
 
 export async function load_all(dir) {
-    return await Promise.all(
-        (await recursive(dir)).map((item) => require(item))
-    );
+    return (
+        await Promise.all((await recursive(dir)).map((item) => require(item)))
+    ).flat();
 }
 
 export function is_string(object) {
